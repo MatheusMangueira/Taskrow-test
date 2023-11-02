f868a69e394696f76c8a6ab0a89a5bf00d098602
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-11-01 22:50:33 -0300

    fix:  update

--- Parte3/react-application/src/app/layout.tsx
+++ Parte3/react-application/src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Navbar } from './components/NavBar/NavBar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'React Application',
-  description: '',
-}
+  description: ''
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="flex justify-center">{children}</div>
+      </body>
     </html>
-  )
+  );
 }
--- Parte3/react-application/src/app/page.tsx
+++ Parte3/react-application/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <div>home page</div>;
+  return (
+    <main className="lg:px-10 p-2 max-w-[1920px] flex justify-center w-full">
+      <div className="flex flex-col w-full">home page</div>
+    </main>
+  );
 }
