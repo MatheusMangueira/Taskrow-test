795bb51fd4396243c5d6c1261c23415dada05a53
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-11-01 22:47:42 -0300

    feat: create hook for number validade Xpto

---
+++ Parte3/react-application/src/app/hooks/useXptoValidation.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { calculateDV, validateNumberXpto } from "../utils/numberXpto";
+
+
+export const useXptoValidation = (numberXPTO: string) => {
+  const [errorsNumberXPTO, setErrorsNumberXPTO] = useState<string>('');
+  const formatNumber = Number(numberXPTO.replace(/-/g, ''));
+
+  const handleNumberXPTO = () => {
+    const numStr: string = numberXPTO.toString();
+    const semDV: number = parseInt(numStr.slice(0, -1).replace(/-/g, ''));
+    if (validateNumberXpto(formatNumber) && calculateDV(semDV)) {
+      setErrorsNumberXPTO('');
+    } else {
+      setErrorsNumberXPTO('Número XPTO inválido');
+    }
+  };
+
+
+  const validateXpto = () => {
+    console.log(numberXPTO + ' teeste ' + numberXPTO.length)
+    if (
+      numberXPTO.length === 6 &&
+      formatNumber >= 10004 &&
+      formatNumber <= 99995
+    ) {
+      setErrorsNumberXPTO('');
+      handleNumberXPTO();
+    }
+    else {
+      setErrorsNumberXPTO('O número XPTO deve estar na faixa de 10004 a 99995');
+    }
+  }
+
+
+  return { validateXpto, errorsNumberXPTO };
+}
+
