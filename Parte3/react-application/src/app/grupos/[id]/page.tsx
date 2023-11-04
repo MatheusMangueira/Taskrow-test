3148081c55e22ce1136b77f3914ccc5e2fa679d6
matheus <matheus.mangueira@teamsoft.com.br>
2023-11-04 01:14:55 -0300

    feat: create page details

---
+++ Parte3/react-application/src/app/grupos/[id]/page.tsx
@@ -0,0 +1,50 @@
+import { groupManager } from '@/lib/groups';
+import { notFound } from 'next/navigation';
+
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+export default function GroupDetails({ params }: Params) {
+  const groups = groupManager.searchGroup('idGrupo', Number(params.id));
+  const users = groupManager.searchUser('nome', groups[0].nome);
+
+  console.log(users);
+
+  if (!users[0]) {
+    notFound();
+  }
+
+  return (
+    <div className="lg:px-10 p-2 max-w-[1920px] h-[calc(100vh-88px)] flex flex-col w-full ">
+      <div className="w-full flex justify-center items-center pb-5">
+        <h1 className="text-4xl pt-5 font-bold">
+          {' '}
+          Detalhes individuais dos grupos
+        </h1>
+      </div>
+      <div className="bg-green-500 p-5 rounded-xl">
+        {users.map((item) => (
+          <div key={item.idGrupo}>
+            <h4 className="text-3xl font-normal">
+              Nome do Grupo: <span className="font-bold">{item.nome}</span>
+            </h4>
+            {item.usuarios?.map((usuario) => (
+              <div key={usuario.idUsuario}>
+                <p className="text-lg p-2 ">
+                  Nome do usuario:
+                  <span className="font-bold">{usuario.nome}</span>
+                  <br />
+                  ID do usuario:
+                  <span className="font-bold">{usuario.idUsuario}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
