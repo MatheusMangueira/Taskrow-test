ef5140b2879d3109633ba3e67091bed4bcecd4af
matheus <matheus.mangueira@teamsoft.com.br>
2023-11-04 01:12:05 -0300

    fix: changing type

--- Parte1/busca_em_estrutura_de_grupos/src/types/grupo.ts
+++ Parte1/busca_em_estrutura_de_grupos/src/types/grupo.ts
@@ -1,12 +1,12 @@
 import { Usuario } from "./usuario";
 
 export interface Grupo {
-   idGrupo: number;
-   nome: string;
-   usuarios: Usuario[];
-   subGrupos: Grupo[];
+  idGrupo: number;
+  nome: string;
+  usuarios: Usuario[];
+  subGrupos?: Grupo[];
 }
 
 export interface GruposJSON {
-   grupos: Grupo[];
+  grupos: Grupo[];
 }
