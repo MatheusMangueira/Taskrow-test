dede1c15a47cf71a9a770f94c8438d7cd3fe627f
matheus <matheus.mangueira@teamsoft.com.br>
2023-11-06 15:06:22 -0300

    bugFix: update

--- Parte3/react-application/src/app/utils/numberXpto.ts
+++ Parte3/react-application/src/app/utils/numberXpto.ts
@@ -1,11 +1,13 @@
-
 export const calculateDV = (num: number) => {
   if (num.toString().length !== 4) {
     throw new Error('Numero precisa ter 4 digitos');
   }
 
   const weights: number[] = [4, 5, 6, 7];
-  const products: number[] = num.toString().split('').map((d, i) => parseInt(d) * weights[i]);
+  const products: number[] = num
+    .toString()
+    .split('')
+    .map((d, i) => parseInt(d) * weights[i]);
 
   let sum: number = 0;
   for (const val of products) {
@@ -16,7 +18,7 @@ export const calculateDV = (num: number) => {
   const sumSeven: number = restDivision + 7;
   const dv: number = sumSeven % 10;
   return dv;
-}
+};
 
 export const validateNumberXpto = (num: number): boolean => {
   const numStr: string = num.toString();
@@ -31,4 +33,4 @@ export const validateNumberXpto = (num: number): boolean => {
   const dv: number = parseInt(numStr[numStr.length - 1]);
   const semDV: number = parseInt(numStr.slice(0, -1));
   return calculateDV(semDV) === dv;
-}
+};
